31b7cdf212375a80135900235ba5e71b6a5f4a09
陈金伙 <chenjinhuo@bytedance.com>
2023-11-05 16:07:12 +0800

    chore(rust-bindings): :hammer: add comment

--- rust/rust-bindings/src/lib.rs
+++ rust/rust-bindings/src/lib.rs
@@ -14,7 +14,11 @@ pub fn get_max_open_file_num() -> i64 {
       rlim_cur: 0,
       rlim_max: 0,
     };
+    // unsafe {
+    //   setrlimit(RLIMIT_NOFILE, &mut limit);
+    // }
     if unsafe { getrlimit(RLIMIT_NOFILE, &mut limit) } == 0 {
+      // 为什么用 napi 时，返回的 rlim_cur 是1048575，与默认值 104960 有差异
       let max_open_file_num = if limit.rlim_cur == RLIM_INFINITY {
         NOT_SUPPORT_FLAG
       } else {
