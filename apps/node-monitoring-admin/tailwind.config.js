1e480eefa1cdea34cf1f39a22a9cc848a28af9eb
陈金伙 <chenjinhuo@bytedance.com>
2023-11-08 13:36:50 +0800

    chore(node-monitoring-admin): :hammer: add postcss

---
+++ apps/node-monitoring-admin/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  important: 'html',
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- apps/node-monitoring-admin/vite.config.ts
+++ apps/node-monitoring-admin/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javaScriptEnabled: true,
+      },
+    },
+  },
 })
