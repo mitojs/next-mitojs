b2e31f416c15fd4e2bc564baa329f32d6157dca8
陈金伙 <chenjinhuo@bytedance.com>
2023-11-08 11:07:57 +0800

    feat(architecture): :sparkles: test for esm

--- packages/node-memory/src/index.ts
+++ packages/node-memory/src/index.ts
@@ -1,3 +1,11 @@
+import fs from 'fs'
+import work from 'node:worker_threads'
 export const add = (a: number, b: number) => {
+  console.log(
+    fs.readFile('./index.ts', () => {
+      new work.MessagePort()
+      console.log(111)
+    }),
+  )
   return a + b
 }
