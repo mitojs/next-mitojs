364403eb3ec4931dbc2b699aba566d260cfcb2ca
陈金伙 <chenjinhuo@bytedance.com>
2024-07-30 12:41:18 +0800

    perf(utils): ⚡️ optimize shimmer

--- packages/utils/src/shimmer.ts
+++ packages/utils/src/shimmer.ts
@@ -2,33 +2,23 @@
 import { isFunction } from './is'
 import { AnyObject } from '@mitojs/types'
 
-const WRAPPED = '__wrapped__'
-const UNWRAP = '__unwrap__'
-const ORIGINAL = '__original__'
-
-/**
- * This interface defines the params that are be added to the wrapped function
- * using the "shimmer.wrap"
- */
-export interface ShimWrapped extends Function {
-  [WRAPPED]: boolean
-  [UNWRAP]: Function
-  [ORIGINAL]: Function
-}
+export const WRAPPED = '__wrapped__'
+export const UNWRAP = '__unwrap__'
+export const ORIGINAL = '__original__'
 
 // Default to complaining loudly when things don't go according to plan.
 const logger = console.error.bind(console)
 
 // Sets a property on an object, preserving its enumerability.
 // This function assumes that the property is already writable.
-function defineProperty(obj: AnyObject, name: string, value: unknown) {
+export function defineProperty(obj: AnyObject, name: string, value: unknown) {
   // eslint-disable-next-line no-prototype-builtins
   const enumerable = !!obj[name] && obj.propertyIsEnumerable(name)
   Object.defineProperty(obj, name, {
     configurable: true,
-    enumerable: enumerable,
+    enumerable,
     writable: true,
-    value: value,
+    value,
   })
 }
 
@@ -55,40 +45,23 @@ export function wrap<T extends AnyObject, K extends keyof T>(nodule: T, name: K,
   // copy enumerable properties
   for (const enumerableKey in original) {
     if (Object.prototype.hasOwnProperty.call(original, enumerableKey)) {
-      defineProperty(wrapped, enumerableKey, original[enumerableKey])
+      wrapped[enumerableKey] = original[enumerableKey]
     }
   }
 
   defineProperty(wrapped, ORIGINAL, original)
+  // prepare for unwrapping
   defineProperty(wrapped, UNWRAP, function () {
-    if (nodule[name] === wrapped) defineProperty(nodule, name as string, original)
+    // If no one else wrap it again, it can be "unwrap" when call unwrap function
+    if (nodule[name] === wrapped) {
+      defineProperty(nodule, name as string, original)
+    }
   })
   defineProperty(wrapped, WRAPPED, true)
 
   // hook original function
   defineProperty(nodule, name as string, wrapped)
   return wrapped
-}
-
-export function massWrap(nodules: AnyObject[], names: string, wrapper: Function) {
-  if (!nodules) {
-    logger('must provide one or more modules to patch')
-    logger(new Error().stack)
-    return
-  } else if (!Array.isArray(nodules)) {
-    nodules = [nodules]
-  }
-
-  if (!(names && Array.isArray(names))) {
-    logger('must provide one or more functions to wrap on modules')
-    return
-  }
-
-  nodules.forEach(function (nodule: AnyObject) {
-    names.forEach(function (name) {
-      wrap(nodule, name, wrapper as any)
-    })
-  })
 }
 
 export function unwrap(nodule: AnyObject, name: string) {
@@ -103,34 +76,4 @@ export function unwrap(nodule: AnyObject, name: string) {
   } else {
     return nodule[name][UNWRAP]()
   }
-}
-
-export function massUnwrap(nodules: AnyObject[], names: string) {
-  if (!nodules) {
-    logger('must provide one or more modules to patch')
-    logger(new Error().stack)
-    return
-  } else if (!Array.isArray(nodules)) {
-    nodules = [nodules]
-  }
-
-  if (!(names && Array.isArray(names))) {
-    logger('must provide one or more functions to unwrap on modules')
-    return
-  }
-
-  nodules.forEach(function (nodule) {
-    names.forEach(function (name) {
-      unwrap(nodule, name)
-    })
-  })
-}
-
-export function isWrapped(func: unknown): func is ShimWrapped {
-  return (
-    typeof func === 'function' &&
-    typeof (func as ShimWrapped)[ORIGINAL] === 'function' &&
-    typeof (func as ShimWrapped)[UNWRAP] === 'function' &&
-    (func as ShimWrapped)[WRAPPED] === true
-  )
 }
